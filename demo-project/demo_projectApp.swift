5def7fd7732da98e9b2adefac21011f11bf5a46f
lucky0165 <sape0165@gmail.com>
2021-04-23 17:21:48 +0200

    Initial Commit

---
+++ demo-project/demo_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_projectApp.swift
+//  demo-project
+//
+//  Created by Łukasz Rajczewski on 23/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct demo_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
